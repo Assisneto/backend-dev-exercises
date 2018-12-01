205e5e8907e60b50bd83a10607eccaee71cfd0c6
Assisneto <netodamasceno12@gmail.com>
2018-12-01 01:32:16 -0300

    task 1

--- task/index.js
+++ task/index.js
@@ -7,13 +7,22 @@ let db = new sqlite3.Database('../exercise01.sqlite', sqlite3.OPEN_READWRITE, (e
   console.log('Deu certo')
 })
 
-db.serialize(() => {
-  db.each(`SELECT id From records`, (err,row)=>{
-    if(err) console.log(err.message);
-    console.log(row.id+"\t");
-  });
+ let sql = `select * from ((((((((records inner join countries on records.country_id = countries.id) as result1
+ inner join education_levels on result1.education_level_id = education_levels.id) as result2
+ inner join marital_statuses on result2.relationship_id = marital_statuses.id) as result3
+ inner join occupations on result3.occupation_id = occupations.id) as result4
+ inner join races on result4.race_id = races.id) as result5
+ inner join relationships on result5.relationship_id = relationships.id) as result6
+ inner join sexes on result6.sex_id = sexes.id) as result7
+ inner join workclasses on result7.workclass_id = workclasses.id)
+`
 
-});
+    db.each(sql,[],(err,row)=>{
+      if(err) console.log(err.message);
+      console.log(row)
+    
+   });
+
 
 
 db.close((err) => {
